4fc4f9becc785916827fd101f2227616451ccdbf
BobBieNeno <bie007x@gmail.com>
2024-10-05 18:44:35 +0700

    set state

---
+++ lib/page/User_send.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class UserSendPage extends StatefulWidget {
+  const UserSendPage({super.key});
+
+  @override
+  State<UserSendPage> createState() => _UserSendPageState();
+}
+
+class _UserSendPageState extends State<UserSendPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(),
+    );
+  }
+}
