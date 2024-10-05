c505f63364e664ad79ab85301dbc9fa2d99a5cc4
BobBieNeno <bie007x@gmail.com>
2024-10-05 19:06:00 +0700

    setstate

---
+++ lib/page/User_Map.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class UserMapPage extends StatefulWidget {
+  const UserMapPage({super.key});
+
+  @override
+  State<UserMapPage> createState() => _UserMapPageState();
+}
+
+class _UserMapPageState extends State<UserMapPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(),
+    );
+  }
+}
