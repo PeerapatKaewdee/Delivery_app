1adca4c82238966e57235b3b682fcf5d9603195c
BobBieNeno <bie007x@gmail.com>
2024-10-06 22:27:34 +0700

    set state

---
+++ lib/page/Risers_Get.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class RisersGetPage extends StatefulWidget {
+  const RisersGetPage({super.key});
+
+  @override
+  State<RisersGetPage> createState() => _RisersGetPageState();
+}
+
+class _RisersGetPageState extends State<RisersGetPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(),
+    );
+  }
+}
