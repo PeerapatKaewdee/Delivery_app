cf076f845715bb32b4911e8d6405a4f9bbd13806
BobBieNeno <bie007x@gmail.com>
2024-10-19 03:49:40 +0700

    set state

---
+++ lib/page/Rider_Pictuer.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class RiderPictuerPage extends StatefulWidget {
+  const RiderPictuerPage({super.key});
+
+  @override
+  State<RiderPictuerPage> createState() => _RiderPictuerPageState();
+}
+
+class _RiderPictuerPageState extends State<RiderPictuerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(),
+    );
+  }
+}
